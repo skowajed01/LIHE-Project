7e2c92d46909a8dc55af1712d46c0e7c25a9a256
SK <skowajed786@gmail.com>
2023-11-04 10:44:08 +0530

    country cng

--- src/Master/ViewCountryMaster.js
+++ src/Master/ViewCountryMaster.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import AllServices from 'services/AllServices';
+
+const services = new AllServices();
 
 class ViewCountryMaster extends Component {
+
+    componentWillUnmount() {
+        console.log("Component will mounting");
+        this.GetCountryList();
+    }
+
+    GetCountryList() {
+        services.ViewCountryList().then((data) => {
+            console.log(data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     render() {
         return (
             <div>
