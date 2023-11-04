2cee2761c601f0c3f867cba17976b328d117c31f
dipankar <dipankargarai420@gmail.com>
2023-11-04 12:52:49 +0530

    view country Api

--- src/Master/ViewCountryMaster.js
+++ src/Master/ViewCountryMaster.js
@@ -1,45 +1,44 @@
 import DataTable from "layout/DataTable/DataTable";
-import React from "react";
+import React, { useEffect } from "react";
 import AllServices from "services/AllServices";
+
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
   {
-    field: "img",
-    headerName: "Avatar",
-    width: 100,
-    renderCell: (params) => {
-      return <img src={params.row.img || "/noavatar.png"} alt="" />;
-    },
+    field: "id",
+    headerName: "Id",
+    width: 90,
   },
   {
-    field: "firstName",
+    field: "countryname",
+    headerName: "CountryName",
+    width: 150,
     type: "string",
-    headerName: "First name",
+  },
+  //   {
+  //     field: "img",
+  //     headerName: "Avatar",
+  //     width: 100,
+  //     renderCell: (params) => {
+  //       return <img src={params.row.img || "/noavatar.png"} alt="" />;
+  //     },
+  //   },
+  {
+    field: "currency",
+    type: "string",
+    headerName: "Currency",
     width: 150,
   },
   {
-    field: "lastName",
+    field: "nationalityname",
     type: "string",
-    headerName: "Last name",
+    headerName: "Nationality Name",
     width: 150,
   },
   {
-    field: "email",
+    field: "callingcode",
     type: "string",
-    headerName: "Email",
+    headerName: "Calling Code",
     width: 200,
-  },
-  {
-    field: "phone",
-    type: "string",
-    headerName: "Phone",
-    width: 200,
-  },
-  {
-    field: "createdAt",
-    headerName: "Created At",
-    width: 200,
-    type: "string",
   },
   //   {
   //     field: "verified",
@@ -49,166 +48,46 @@ const columns = [
   //   },
 ];
 
-const userRows = [
-  {
-    id: 1,
-    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    lastName: "Hubbard",
-    firstName: "Eula",
-    email: "kewez@@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 2,
-    img: "https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Manning",
-    firstName: "Stella",
-    email: "comhuhmit@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 3,
-    img: "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Greer",
-    firstName: "Mary",
-    email: "ujudokon@hottmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 4,
-    img: "https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Williamson",
-    firstName: "Mildred",
-    email: "tinhavabe@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 5,
-    img: "https://images.pexels.com/photos/1758144/pexels-photo-1758144.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Gross",
-    firstName: "Jose",
-    email: "gobtagbes@yahoo.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 6,
-    img: "https://images.pexels.com/photos/769745/pexels-photo-769745.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Sharp",
-    firstName: "Jeremy",
-    email: "vulca.eder@mail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 7,
-    img: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Lowe",
-    firstName: "Christina",
-    email: "reso.bilic@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 8,
-    img: "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Dean",
-    firstName: "Garrett",
-    email: "codaic@mail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 9,
-    img: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Parsons",
-    firstName: "Leah",
-    email: "uzozor@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 10,
-    img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Reid",
-    firstName: "Elnora",
-    email: "tuhkabapu@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 11,
-    img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Dunn",
-    firstName: "Gertrude",
-    email: "gibo@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-    verified: true,
-  },
-  {
-    id: 12,
-    img: "https://images.pexels.com/photos/774095/pexels-photo-774095.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Williams",
-    firstName: "Mark",
-    email: "tic.harvey@hotmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 13,
-    img: "https://images.pexels.com/photos/761977/pexels-photo-761977.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Cruz",
-    firstName: "Charlotte",
-    email: "ceuc@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 14,
-    img: "https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    lastName: "Harper",
-    firstName: "Sara",
-    email: "bafuv@hotmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-  {
-    id: 15,
-    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    lastName: "Griffin",
-    firstName: "Eric",
-    email: "ubi@gmail.com",
-    phone: "123 456 789",
-    createdAt: "01.02.2023",
-  },
-];
-
+// const userRows = [
+//   {
+//     id: "1",
+//     countryname: "Malaysia",
+//     currency: "MYR",
+//     nationalityname: "Malaysian",
+//     callingcode: "61",
+//   },
+// ];
+var countryLists;
+var service = new AllServices();
 const ViewCountryMaster = () => {
-  // componentWillUnmount() {
-  //     console.log("Component will mounting");
-  //     this.GetCountryList();
-  // }
+  useEffect(() => {
+    console.log("fetching api");
+    GetCountryList();
+  });
 
-  // GetCountryList() {
-  //     services.ViewCountryList().then((data) => {
-  //         console.log(data)
-  //     }).catch((error) => {
-  //         console.log(error)
-  //     })
-  // }
+  const GetCountryList = () => {
+    service
+      .ViewCountryList()
+      .then((data) => {
+        countryLists = data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  var countryList = [];
+
+  countryLists.forEach((elements, index, array) => {
+    var item = {};
+    item.countryname = elements.countryname;
+    item.currency = elements.currency;
+    item.nationalityname = elements.nationalityname;
+    item.callingcode = elements.callingcode;
+    countryList.push(item);
+  });
+  const userRows = countryList;
   return (
     <>
       <div className="users">
